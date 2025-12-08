51e8267e2377bc921e19f383f8f9237474d5df57
rlucasorsi <rlucasorsi@gmail.com>
2025-12-08 17:32:56 -0300

    adjust label button

--- 3_AVANÇANDO_COM_REACT/avancando/src/components/ChangeMessage.jsx
+++ 3_AVANÇANDO_COM_REACT/avancando/src/components/ChangeMessage.jsx
@@ -2,9 +2,9 @@ const ChangeMessage = ({handleMessage}) => {
     const messages = ["Oi", "Olá", "Tudo bem?"]
 
   return <div>
-    <button onClick={() => handleMessage(messages[0])}>1</button>
-    <button onClick={() => handleMessage(messages[1])}>2</button>
-    <button onClick={() => handleMessage(messages[2])}>3</button>
+    <button onClick={() => handleMessage(messages[0])}>Oi</button>
+    <button onClick={() => handleMessage(messages[1])}>Olá</button>
+    <button onClick={() => handleMessage(messages[2])}>Tudo bem?</button>
   </div>;
 };
 
