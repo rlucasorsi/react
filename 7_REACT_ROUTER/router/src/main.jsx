5a12c7ceba99fc1a84cf976cfafb1166c76396e2
Lucas Orsi <lucas.orsi@contractlabs.tech>
2026-03-31 14:08:51 -0300

    feat: create base layout component with Outlet

--- 7_REACT_ROUTER/router/src/main.jsx
+++ 7_REACT_ROUTER/router/src/main.jsx
@@ -3,21 +3,42 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
+// Pagina de contato
+import Contact from './routes/Contact.jsx';
+
+// Pagina de erro
+import ErrorPage from './routes/ErrorPage.jsx';
+
+// Pagina de home
+import Home from './routes/Home.jsx';
+
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
+    // 3- Componente base
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+    ],
   },
-  {
-    path: 'contact',
-    element: <Contact />
-  }
+  // {
+  //   path: 'contact',
+  //   element: <Contact />,
+  // },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
